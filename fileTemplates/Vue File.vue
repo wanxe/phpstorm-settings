1ca83bf06866db588f42fee6f7a94df3c22097b8
JNieto <jorge.bbz@gmail.com>
2017-07-19 22:40:57 +0100

    PS-2017.2 <jnieto@MacBook-Pro.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, git.xml, other.xml, vagrant.xml, Default.xml, Json.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, php-test-framework.xml, code.style.schemes.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, Vue File.vue, databaseDrivers.xml, github_settings.xml, Material Wanxez.icls, customization.xml, Default.xml, PHP.xml, JavaScript.xml, Material Theme - Default copy.icls, JNieto-Theme.icls, intentionSettings.xml, laf.xml, web-browsers.xml, material_theme.xml, Jnieto.xml, editor.codeinsight.xml, databaseSettings.xml, keymap.xml, IntelliJ IDEA Classic _OS X_ copy.xml, SublimeText3.xml, editor.xml, usageView.xml, CSS.xml, debugger.xml

---
+++ fileTemplates/Vue File.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {}
+  }
+</script>
+
+<style lang="css" scoped>
+    body {
+        background-color: #ff0000;
+    }
+</style>
